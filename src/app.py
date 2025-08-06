358cd95f708285881b6d193a8011e5d5949abee8
sameerdshar <40250073+sameerdshar@users.noreply.github.com>
2025-08-06 13:41:33 -0500

    Create app (#13)
    * update dockerfile and requirements (#7)
    
    * removing pyobjc
    
    * updating packages
    
    * retrying packaging
    
    * Sync (#11)
    
    * Update requirements.txt (#8)
    
    * update dockerfile and requirements (#7)
    
    * removing pyobjc
    
    * Create app (#9)
    
    * update dockerfile and requirements (#7)
    
    * removing pyobjc
    
    * updating packages
    
    * Create app (#10)
    
    * update dockerfile and requirements (#7)
    
    * removing pyobjc
    
    * updating packages
    
    * retrying packaging
    
    * Update port number
    
    * Update app.py

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import logging
 from get_Zone import get_zone
 from datetime import date
 from datetime import datetime as dt
+import os
 
 logging.basicConfig(
     level=logging.INFO,
@@ -64,4 +65,5 @@ def index():
     return render_template('index.html', coords=coords)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
