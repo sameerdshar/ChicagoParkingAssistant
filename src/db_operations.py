14829142ac41d75197a6c9b188bdf627cb646a84
Sameer <sameer.dc.sharma@gmail.com>
2025-08-06 14:25:46 -0500

    Updating app details

--- src/db_operations.py
+++ src/db_operations.py
@@ -43,6 +43,6 @@ def create_table(table_name: str, data: pd.DataFrame) -> None:
     engine = get_engine()
     query = """CREATE SCHEMA IF NOT EXISTS CPA;"""
     with engine.connect() as connection:
-        connection.execute(query)
+        connection.execute(sa.text(query))
     data.to_sql(table_name, con=engine, schema='CPA', if_exists='replace', index=False)
     
